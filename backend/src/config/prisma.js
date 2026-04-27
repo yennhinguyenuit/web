1cdfe23047aa9510f2cb5c8be4cb6f56453e8782
yennhinguyenuit <yennhinguyen.uit@gmail.com>
2026-04-27 14:25:14 +0700

    nhi

--- backend/src/config/prisma.js
+++ backend/src/config/prisma.js
@@ -1,25 +1,11 @@
 require("dotenv").config();
 const { PrismaClient } = require("../generated/prisma");
-const { PrismaPg } = require("@prisma/adapter-pg");
-const { Pool } = require("pg");
 
 const globalForPrisma = global;
 
-const pool =
-  globalForPrisma.pgPool ||
-  new Pool({
-    connectionString: process.env.DATABASE_URL,
-  });
-
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.pgPool = pool;
-}
-
-const adapter = new PrismaPg(pool);
-
 const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient({ adapter });
+  new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
