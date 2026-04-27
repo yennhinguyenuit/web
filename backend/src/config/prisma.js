3afb54d1c0652e91147c4ad8a93768d0b6c68377
yennhinguyenuit <yennhinguyen.uit@gmail.com>
2026-04-27 21:38:39 +0700

    use defau

--- backend/src/config/prisma.js
+++ backend/src/config/prisma.js
@@ -5,7 +5,11 @@ const globalForPrisma = globalThis;
 const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    datasourceUrl: process.env.DATABASE_URL,
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
   });
 
 if (process.env.NODE_ENV !== "production") {
